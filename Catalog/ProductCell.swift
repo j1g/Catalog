906231d5450d5333f644cd210bb50066b433b8e5
sdt5 <sdt5@C3-7ui-MacBook-Pro.local>
2015-10-22 16:17:41 +0900

    init

---
+++ Catalog/ProductCell.swift
@@ -0,0 +1,37 @@
+//
+//  ProductCell.swift
+//  Catalog
+//
+//  Created by sdt5 on 2015. 10. 22..
+//  Copyright © 2015년 j1g. All rights reserved.
+//
+
+import UIKit
+
+protocol ProductCellDelegate: class {
+    func addCart(ProductCode: String)
+    
+}
+class ProductCell: UITableViewCell {
+    
+    weak var delegate: ProductCellDelegate!
+    var productCode: String!
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBAction func addCart(sender: AnyObject) {
+        print("CART")
+        delegate.addCart(productCode)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Catalog/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Catalog
+//
+//  Created by sdt5 on 2015. 10. 22..
+//  Copyright © 2015년 j1g. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ProductCellDelegate {
+    
+    func addCart(productCode: String) {
+        print("CART DELEGATE ")
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var balls = ["Baseball", "Basketball", "Football", "Golf", "Rugby", "Shuttlecock", "Tennis", "Volleyball", "Billiard", "Icehockey", "Pingpong"]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return balls.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        //custom cell
+        let cell = tableView.dequeueReusableCellWithIdentifier("PRODUCT_CELL") as! ProductCell
+        //cell.nameLabel.text = "Row \(indexPath.row)"
+        
+        
+        cell.nameLabel.text = balls[indexPath.row]
+        let itemName = balls[indexPath.row]
+        
+        let image = UIImage(named: "\(itemName).png")
+        cell.imageView?.image = image
+        
+        //attribute ->  table view cell: style
+        //let detail = "Row: \(indexPath.row)"
+        //cell.detailTextLabel?.text = detail
+        
+        cell.productCode = "\(indexPath.row)"
+        cell.delegate = self
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("selected \(indexPath.row)")
+        
+        //셀 선택 후 배경색(회색: 선택했다는 표시) 사라짐.
+        //tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

286765824b09be9b59011c09e708d953f86f391c
sdt5 <sdt5@C3-7ui-MacBook-Pro.local>
2015-10-22 17:43:06 +0900

    Added the 2nd section.

--- Catalog/ViewController.swift
+++ Catalog/ViewController.swift
@@ -10,41 +10,77 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ProductCellDelegate {
     
-    func addCart(productCode: String) {
-        print("CART DELEGATE ")
-    }
     
     @IBOutlet weak var tableView: UITableView!
     
-    
     var balls = ["Baseball", "Basketball", "Football", "Golf", "Rugby", "Shuttlecock", "Tennis", "Volleyball", "Billiard", "Icehockey", "Pingpong"]
-    var prices = ["100", "300", "400", "200", "500", "900", "600", "700", "800", "1000", "1100"]
+    var prices = ["100", "300", "400", "200", "500", "1100", "900", "600", "800", "1000", "700"]
+    var cart = [String]()
+    
+
+    func addCart(productCode: String) {
+        print("\(productCode) added.")
+        cart.insert(productCode, atIndex: 0)
+        //cart.append(productCode)
+        tableView.reloadData()
+    }
+
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // 1
+        // Return the number of sections.
+        return 2
+    }
+
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        /*switch section {
+        case 0:
+            return "Catalog"
+        }*/
+        
+        if section == 0 {
+            return "Catalog"
+        } else {
+            return "Cart"
+        }
+    }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return balls.count
+        if section == 0 {
+            return balls.count
+        } else {
+            return cart.count
+        }
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0 {
+        
+            //custom cell
+            let cell = tableView.dequeueReusableCellWithIdentifier("PRODUCT_CELL") as! ProductCell
+            //cell.nameLabel.text = "Row \(indexPath.row)"
+            cell.nameLabel.text = balls[indexPath.row]
 
-        //custom cell
-        let cell = tableView.dequeueReusableCellWithIdentifier("PRODUCT_CELL") as! ProductCell
-        //cell.nameLabel.text = "Row \(indexPath.row)"
+            cell.priceLabel.text = prices[indexPath.row]
+            
+            let itemName = balls[indexPath.row]
+            let image = UIImage(named: "\(itemName).png")
+            cell.imageView?.image = image
         
-        
-        cell.nameLabel.text = balls[indexPath.row]
-        cell.priceLabel.text = prices[indexPath.row]
-        let itemName = balls[indexPath.row]
-        
-        let image = UIImage(named: "\(itemName).png")
-        cell.imageView?.image = image
-        
-        //attribute ->  table view cell: style
-        //let detail = "Row: \(indexPath.row)"
-        //cell.detailTextLabel?.text = detail
-        
-        cell.productCode = "\(indexPath.row)"
-        cell.delegate = self
-        
-        return cell
+            //cell.productCode = "\(indexPath.row)"
+            cell.productCode = balls[indexPath.row]
+            cell.delegate = self
+
+            return cell
+        }
+        else {
+            //print("owiejfoiwejfoiwejfiojweiof")
+            let cellCart = tableView.dequeueReusableCellWithIdentifier("CART_CELL")!
+            cellCart.textLabel?.text = cart[indexPath.row]
+            
+            return cellCart
+        }
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {

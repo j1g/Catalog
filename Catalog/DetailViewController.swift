c3fab7d4e02f628c166068da95c45a88068b66d7
j1g <sdt5@C3-7ui-MacBook-Pro.local>
2015-10-26 15:56:24 +0900

    Added Navigation- /Tab Bar Controller.

---
+++ Catalog/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  Catalog
+//
+//  Created by sdt5 on 2015. 10. 26..
+//  Copyright © 2015년 j1g. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

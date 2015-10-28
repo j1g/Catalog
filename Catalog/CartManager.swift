36c7728e9dc37669f6cf976dc17978e6ea3f3f95
j1 <jaehwan0419@gmail.com>
2015-10-28 16:57:55 +0900

    Added function: Names of added products are shown on the CartTable.

--- Catalog/CartManager.swift
+++ Catalog/CartManager.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import RealmSwift
+import Alamofire
 
 class CartManager {
     
@@ -31,15 +32,65 @@ class CartManager {
         let cartItem = CartItem()
         cartItem.name = productCode
         
-        // Save in DB
+        // Save in local DB
         //let realm = try! Realm()
         try! realm.write {
             self.realm.add(cartItem)    // query
         }
+        
+        // Save in online DB
+        sendRequest(productCode)
+        
     }
     
     func loadDB() {
         items = realm.objects(CartItem)     // query
-        print(items)
+        //print(items)
+    }
+    
+    
+    func sendRequest(productCode: String) {
+        
+        // Create manager
+        let manager = Manager.sharedInstance
+        
+        // Add Headers
+        manager.session.configuration.HTTPAdditionalHeaders = [
+            "X-Parse-Application-ID":"PzVoV4C9uJiOt6ZLoSXjtaRXYmc0MY97Pudbyxiy",
+            "X-Parse-REST-API-Key":"ew8LXDIujLFOtZBqkiHjDNUhnLHLHhAyQZ0VHFK2",
+            "Content-Type":"application/json",
+        ]
+        
+        let headers = [
+            "X-Parse-Application-ID":"PzVoV4C9uJiOt6ZLoSXjtaRXYmc0MY97Pudbyxiy",
+            "X-Parse-REST-API-Key":"ew8LXDIujLFOtZBqkiHjDNUhnLHLHhAyQZ0VHFK2",
+            "Content-Type":"application/json",
+        ]
+        
+        // JSON Body
+        let bodyParameters = [
+            "name": "\(productCode)"
+        ]   //"name": "Volleyball"
+        
+        print("Body Parameter: \(bodyParameters)")
+        
+        let encoding = Alamofire.ParameterEncoding.JSON
+        
+        // Fetch Request
+        Alamofire.request(.POST, "https://api.parse.com/1/classes/CartItem", parameters: bodyParameters, encoding: encoding, headers: headers)
+            .validate(statusCode: 200..<300)
+            .responseJSON { response in
+                print("\nSucces or Fail ???")
+                
+                switch response.result {
+                case .Success:
+                    print("Validation Successful")
+                case .Failure(let error):
+                    print(error)
+                }
+                
+                print("\nFetched.")
+        }
+        
     }
 }
--- Podfile
+++ Podfile
@@ -3,10 +3,14 @@ use_frameworks!
 
 target 'Catalog' do
 	pod 'RealmSwift'
+	pod 'Alamofire', '~> 3.0'
+	pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
 end
 
 target 'CatalogTests' do
 	pod 'RealmSwift'
+	pod 'Alamofire', '~> 3.0'
+	pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
 end
 
 target 'CatalogUITests' do
